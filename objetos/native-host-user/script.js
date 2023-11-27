e0930cde27696adc96701c415228d18dc4d00cbf
Luiz <luizhmartins11@gmail.com>
2023-11-27 09:55:43 -0300

    estudos sobre native-user-host

---
+++ objetos/native-host-user/script.js
@@ -0,0 +1,20 @@
+// Native = objetos nativos sao aqueles definidos na especificação da linguagem e sao implementados independente do host.
+//Ex : Object - String - Array - Function
+
+//Host = Objetos do host sao aqueles implementados pelo proprio ambiente. Por exemplo no browser possuimos objetos do DOM, como DOMList, HTMLCollection e outros. Em Node.js os objetos do Host sao diferentes, ja que nao estamos em um ambiente do browser
+//Ex : NodeList - HTMLCollection - Element
+
+//User = objetos user, sao os objetos definidos pelo seu aplicativo. Ou seja, qualquer objeto que voce criar ou que importar de alguma biblioteca externa.
+//Ex:
+/**
+ const pessoa = {
+  nome = "Andre",
+  idade = '29';
+ }
+ */
+
+// Bibiliotecas = foram criadas para resolver problemas de inconsistencias entre browser e adicionar funcionalidades que nao existiam nativamente.
+
+//é possivel verificar o tipo de dado utlizando o typeOf
+
+//API = é uma interface de software criada para a interação com outros softwares. * Toda a interação que fazemos com o browser estamos utilizando a API do browser *
